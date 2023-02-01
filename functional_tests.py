6fb0f6342de5b64a55b16a725ef6cb2c400d2892
Felix Stippler <stipplerf@gmail.com>
2023-02-01 16:12:06 +0100

    FT now has new helper method to check text_items

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         #new online to-do app, visit the homepage
@@ -33,10 +38,7 @@ class NewVisitorTest(unittest.TestCase):
         # in list format as '1: buy new shows'
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: buy new shoes', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: buy new shoes')
 
         #the input mask is still there waiting for another item
         #input is 'buy new shoe laces'
@@ -46,11 +48,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         #page updates again and shows both items
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: buy new shoes', [row.text for row in rows])
-        self.assertIn('2: buy new shoe laces', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('1: buy new shoes')
+        self.check_for_row_in_list_table('2: buy new shoe laces')
 
         #returns a unique url with the list for each user
         self.fail('Finish the test!')
