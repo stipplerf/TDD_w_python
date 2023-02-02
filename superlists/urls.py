21d5e94eb58fbd8fcb74562d4da44de78778cd2b
Felix Stippler <stipplerf@gmail.com>
2023-02-02 14:37:43 +0100

    added a new template and view for creating a new list

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from lists import views
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    url(r'^lists/the-one-and-only/$', views.view_list, name='view_list')
+    url(r'^lists/new$', views.new_list, name='new_list'),
+    url(r'^lists/the-one-and-only/$', views.view_list, name='view_list'),
 ]
