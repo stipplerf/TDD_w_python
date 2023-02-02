c16faadf403736fc9ed904ba5f867e7d8a9c58cd
Felix Stippler <stipplerf@gmail.com>
2023-02-02 15:01:51 +0100

    adjusted the model so that items have a list attribute

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class ListViewTest(TestCase):
@@ -14,8 +14,9 @@ class ListViewTest(TestCase):
 
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-one-and-only/')
 
@@ -40,16 +41,24 @@ class HomePageTest(TestCase):
        response = self.client.get('/')   
        self.assertTemplateUsed(response, 'home.html')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -58,3 +67,5 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.list, list_)
