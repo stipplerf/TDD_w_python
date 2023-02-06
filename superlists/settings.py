e5afd299b6c434f52f0b597b50add4d5b163d3ce
Felix Stippler <stipplerf@gmail.com>
2023-02-06 10:49:05 +0100

    hack ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y)h!a#$1o!-9hdd-wytlaskhzx9ngpn0#&^w^95v*oa-s--+o*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
